6fa46a3a42aef1d9b16cfa89bf9a5e098e5289e3
Isael Silva <isaelsilva1039@gmail.com>
2022-03-28 12:48:10 -0300

    ajuste

--- front-end/Views/html/tesoreiro/relatorio.php
+++ front-end/Views/html/tesoreiro/relatorio.php
@@ -233,7 +233,9 @@
             <li class="sidebar-item">
               <a class="sidebar-link waves-effect waves-dark sidebar-link" href="dashbord.php" aria-expanded="false"><i class="mdi mdi-view-dashboard"></i><span class="hide-menu">Principal</span></a>
             </li>
-            
+            <li class="sidebar-item">
+              <a class="sidebar-link waves-effect waves-dark sidebar-link" href="relatorio.php" aria-expanded="false"><i class="mdi mdi-chart-bubble"></i><span class="hide-menu">Relatório</span></a>
+            </li>
             <li class="sidebar-item">
               <a class="sidebar-link waves-effect waves-dark sidebar-link" href="relatorioofertas.php" aria-expanded="false"><i class="mdi mdi-chart-bubble"></i><span class="hide-menu">Relatório Ofertas</span></a>
             </li>
