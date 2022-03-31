180c49d7927b09ba6829e8c941947895a4134669
William Klywerston <williamklywerston10@gmail.com>
2022-03-31 14:26:43 -0300

    atualização

--- front-end/Views/html/tesoreiro/dashbord.php
+++ front-end/Views/html/tesoreiro/dashbord.php
@@ -41,7 +41,7 @@
   <div id="main-wrapper" data-layout="vertical" data-navbarbg="skin5" data-sidebartype="full" data-sidebar-position="absolute" data-header-position="absolute" data-boxed-layout="full">
     <header class="topbar" data-navbarbg="skin5">
       <nav class="navbar top-navbar navbar-expand-md navbar-dark">
-        <div class="navbar-header" data-logobg="skin5">
+        <div class="navbar-header"  style="background-color: #9900aa;">
           <a class="navbar-brand" href="index.php">
             <b class="logo-icon ps-2">
               <img src="https://www.serfautec.com.br/images/igraja-serfautec.jpg" alt="homepage" class="light-logo" style="height: 40px; border-radius: 100px; width: 50px; margin-left: -10px;" />
@@ -52,8 +52,8 @@
           </a>
           <a class="nav-toggler waves-effect waves-light d-block d-md-none" href="javascript:void(0)"><i class="ti-menu ti-close"></i></a>
         </div>
-        <div class="navbar-collapse collapse" id="navbarSupportedContent" data-navbarbg="skin5">
-          <ul class="navbar-nav float-start me-auto">
+        <div class="navbar-collapse collapse" id="navbarSupportedContent" >
+          <ul class="navbar-nav float-start me-auto" style="background-color: #9900aa;">
             <li class="nav-item d-none d-lg-block">
               <a class="nav-link sidebartoggler waves-effect waves-light" href="javascript:void(0)" data-sidebartype="mini-sidebar"><i class="mdi mdi-menu font-24"></i></a>
             </li>
